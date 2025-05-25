7010fc2bd53ee47485de6c2524781f0bfd7cde69
oscar <oscar.burman005@gmail.com>
2025-05-25 16:39:07 +0200

    Made navbar take up full screen width

--- src/components/layout/nav/Navbar.tsx
+++ src/components/layout/nav/Navbar.tsx
@@ -12,29 +12,29 @@ const NavBar = async () => {
   return (
     <div className="bg-background sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-background">
-        <MaxWidthWrapper>
-          <div className="border-b border-muted">
-            <div className="flex items-center justify-between h-16">
-              <Link href="/">
-                <h1 className="text-2xl font-semibold text-text-color tracking-wide">
-                  Env<span className="text-primary tracking-wide">Vault</span>
-                </h1>
-              </Link>
+        {/* <MaxWidthWrapper> */}
+        <div className="border-b border-muted px-8">
+          <div className="flex items-center justify-between h-16">
+            <Link href="/">
+              <h1 className="text-2xl font-semibold text-text-color tracking-wide">
+                Env<span className="text-primary tracking-wide">Vault</span>
+              </h1>
+            </Link>
 
-              {user && (
-                <div>
-                  <Link href="/dashboard" className={buttonVariants({ variant: "secondary" })}>
-                    Dashboard
-                  </Link>
-                </div>
-              )}
-
-              <div className="flex items-center">
-                <NavControls user={user} />
+            {user && (
+              <div>
+                <Link href="/dashboard" className={buttonVariants({ variant: "secondary" })}>
+                  Dashboard
+                </Link>
               </div>
+            )}
+
+            <div className="flex items-center">
+              <NavControls user={user} />
             </div>
           </div>
-        </MaxWidthWrapper>
+        </div>
+        {/* </MaxWidthWrapper> */}
       </header>
     </div>
   );
