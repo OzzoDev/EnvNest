5d1ffa2e64c2644c73f19cea0262b12f1e5a9a30
oscar <oscar.burman005@gmail.com>
2025-05-29 14:48:00 +0200

    Cleaned up the project watcher

--- src/components/ProjectWatcher.tsx
+++ src/components/ProjectWatcher.tsx
@@ -2,13 +2,9 @@
 
 import { useProjectStore } from "@/store/projectStore";
 import { trpc } from "@/trpc/client";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 const ProjectWatcher = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
   const projectId = useProjectStore((state) => state.projectId);
   const setProjectId = useProjectStore((state) => state.setProjectId);
 
@@ -16,20 +12,9 @@ const ProjectWatcher = () => {
 
   useEffect(() => {
     if (!projectId && projects && projects?.length > 0) {
-      setProjectId(projects[0].id.toString());
+      setProjectId(projects[0].id);
     }
   }, [projects]);
-
-  useEffect(() => {
-    if (projectId) {
-      const params = new URLSearchParams(searchParams.toString());
-
-      if (searchParams.get("projectId") !== projectId) {
-        params.set("projectId", projectId);
-        router.push(`${pathname}?${params.toString()}`);
-      }
-    }
-  }, [projectId]);
 
   return null;
 };
