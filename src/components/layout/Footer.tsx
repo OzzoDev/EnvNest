18e23fbb087204ec36d7fbb7ff75f1f70ad1f13f
OzzoDev <oscar.burman005@gmail.com>
2025-07-02 19:19:15 +0200

    Added github links in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,31 @@
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
+
 const Footer = () => {
   return (
     <footer className="w-full border-t py-12 text-center text-sm text-muted-foreground">
-      <p>
-        © {new Date().getFullYear()} EnvNest. By developers, for developers.
-      </p>
+      <div className="flex flex-wrap items-center mx-auto w-fit">
+        <p>
+          © {new Date().getFullYear()}&nbsp;
+          <Link
+            className={cn(buttonVariants({ variant: "link" }), "px-0 text-sm")}
+            href="https://github.com/OzzoDev/EnvNest"
+          >
+            EnvNest.
+          </Link>
+          &nbsp;By developers, for developers.
+        </p>
+      </div>
+      <div>
+        Created by&nbsp;
+        <Link
+          className={cn(buttonVariants({ variant: "link" }), "px-0 text-sm")}
+          href="https://github.com/OzzoDev"
+        >
+          OzzoDev
+        </Link>
+      </div>
     </footer>
   );
 };
