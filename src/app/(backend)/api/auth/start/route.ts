76d78983487f6cd6aa94e0c147340046940b0db3
OzzoDev <oscar.burman005@gmail.com>
2025-07-04 19:20:30 +0200

    No lint errors

--- src/app/(backend)/api/auth/start/route.ts
+++ src/app/(backend)/api/auth/start/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import crypto from "crypto";
 import { OAuthApp } from "@octokit/oauth-app";
 import { sessions } from "../sessionStore";
