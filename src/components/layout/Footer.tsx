2c59aa53ac29f3e1f44fa5dbb21082225f76a7e2
OzzoDev <oscar.burman005@gmail.com>
2025-07-04 16:35:20 +0200

    Updated footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,27 +4,32 @@ import { cn } from "@/lib/utils";
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t py-12 text-center mt-32 text-sm text-muted-foreground">
-      <div className="flex flex-wrap items-center mx-auto w-fit">
-        <p>
-          © {new Date().getFullYear()}&nbsp;
+    <footer className="pt-32">
+      <div className="w-full border-t py-12 text-center text-sm text-muted-foreground">
+        <div className="flex flex-wrap items-center mx-auto w-fit">
+          <p>
+            © {new Date().getFullYear()}&nbsp;
+            <Link
+              className={cn(
+                buttonVariants({ variant: "link" }),
+                "px-0 text-sm"
+              )}
+              href="https://github.com/OzzoDev/EnvNest"
+            >
+              EnvNest.
+            </Link>
+            &nbsp;By developers, for developers.
+          </p>
+        </div>
+        <div>
+          Created by&nbsp;
           <Link
             className={cn(buttonVariants({ variant: "link" }), "px-0 text-sm")}
-            href="https://github.com/OzzoDev/EnvNest"
+            href="https://github.com/OzzoDev"
           >
-            EnvNest.
+            OzzoDev
           </Link>
-          &nbsp;By developers, for developers.
-        </p>
-      </div>
-      <div>
-        Created by&nbsp;
-        <Link
-          className={cn(buttonVariants({ variant: "link" }), "px-0 text-sm")}
-          href="https://github.com/OzzoDev"
-        >
-          OzzoDev
-        </Link>
+        </div>
       </div>
     </footer>
   );
